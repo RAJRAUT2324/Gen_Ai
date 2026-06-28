31f7c082107c3ee68e4d7c79bf5ff2560eac8c42
Raj <rautg7230@gmail.com>
2026-06-28 16:44:40 +0530

    Google_Search_Agent_Devlopment

---
+++ apps/2_Google_serach_agent.py
@@ -0,0 +1,29 @@
+
+# This is Google search agent who can search on google and give result 
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+
+llm=ChatGroq(model="openai/gpt-oss-20b")
+search=GoogleSerperAPIWrapper()
+
+agent=create_agent(
+    model=llm,
+    tools=[search.run],
+    system_prompt="You are aagent who can search any question on google"
+)
+
+while True:
+    query=input("User:")
+    if query.lower()=="quit":
+        print("Good Bye !")
+        break
+    else:
+        response=agent.invoke({"messages":[{"role":"user","content":query}]})
+        print("AI:",response["messages"][-1].content)
+        
